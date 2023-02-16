04923096570eec0b33cdd4412d28dadc916a537e
SebastianJM <buy16@uib.no>
2023-02-16 12:56:17 +0100

    frfr this time added controllable methods to controller

--- src/main/java/inf112/bomberperson/controller/Controller.java
+++ src/main/java/inf112/bomberperson/controller/Controller.java
@@ -1,12 +1,37 @@
-package main.java.inf112.bomberperson.controller;
+package inf112.bomberperson.controller;
 
-
-
-public class Controller implements Controllable{
+/**
+ * Controller
+ */
+public class Controller implements Controllable {
 
     @Override
-    public boolean movePlayer(int deltaRow, int deltaCol){
+    public boolean movePlayer(int deltaRow, int deltaCol) {
+        // TODO Auto-generated method stub
         return false;
     }
 
+    @Override
+    public boolean dropBomb(int row, int col) {
+        // TODO Auto-generated method stub
+        return false;
+    }
+
+    @Override
+    public boolean checkLegalMove() {
+        // TODO Auto-generated method stub
+        return false;
+    }
+
+    @Override
+    public int getInterval() {
+        // TODO Auto-generated method stub
+        return 0;
+    }
+
+    @Override
+    public void clockTick() {
+        // TODO Auto-generated method stub
+        
+    }
 }
