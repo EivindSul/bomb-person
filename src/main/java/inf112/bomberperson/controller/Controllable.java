98fb6d81e0d3fdc29dea8e7fdfd91c6c1c124499
Eivind Sulen <eivindsulen2002@gmail.com>
2023-02-16 12:41:35 +0100

    created methods in controllable interface

--- src/main/java/inf112/bomberperson/controller/Controllable.java
+++ src/main/java/inf112/bomberperson/controller/Controllable.java
@@ -1,8 +1,17 @@
 package main.java.inf112.bomberperson.controller;
 
 
-
+// Based on the solution to INF101 tetris
 public interface Controllable{
 
+
+    public boolean movePlayer(int deltaRow, int deltaCol);
+    public boolean dropBomb(int row, int col);
+    public boolean checkLegalMove();
+
+    public int getInterval();
+    public void clockTick();
+
+    // public GameScreen getGameScreen();
 
 }
--- src/main/java/inf112/bomberperson/controller/Controller.java
+++ src/main/java/inf112/bomberperson/controller/Controller.java
@@ -4,5 +4,9 @@ package main.java.inf112.bomberperson.controller;
 
 public class Controller implements Controllable{
 
+    @Override
+    public boolean movePlayer(int deltaRow, int deltaCol){
+        return false;
+    }
 
 }
