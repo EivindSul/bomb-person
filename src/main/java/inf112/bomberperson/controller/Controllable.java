5838ed2371fb7b002b9154c922f5ffab28a3f870
Eivind Sulen <eivindsulen2002@gmail.com>
2023-02-16 12:32:45 +0100

    created controllable interface and controller class, which implements interface

---
+++ src/main/java/inf112/bomberperson/controller/Controllable.java
@@ -0,0 +1,8 @@
+package main.java.inf112.bomberperson.controller;
+
+
+
+public interface Controllable{
+
+
+}
---
+++ src/main/java/inf112/bomberperson/controller/Controller.java
@@ -0,0 +1,8 @@
+package main.java.inf112.bomberperson.controller;
+
+
+
+public class Controller implements Controllable{
+
+
+}
