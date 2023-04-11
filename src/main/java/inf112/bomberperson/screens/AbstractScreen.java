33e47130f680cf487473350544cc25edaa1f253f
Brian.Jensvoll-Jorgensen <bje007@uib.no>
2023-04-11 13:30:29 +0200

    Removed unused assets

---
+++ src/main/java/inf112/bomberperson/screens/AbstractScreen.java
@@ -0,0 +1,21 @@
+package inf112.bomberperson.screens;
+
+import com.badlogic.gdx.Gdx;
+import com.badlogic.gdx.Screen;
+import com.badlogic.gdx.graphics.Color;
+import com.badlogic.gdx.graphics.GL30;
+import com.badlogic.gdx.graphics.g2d.BitmapFont;
+import com.badlogic.gdx.scenes.scene2d.InputEvent;
+import com.badlogic.gdx.scenes.scene2d.InputListener;
+import com.badlogic.gdx.scenes.scene2d.Stage;
+import com.badlogic.gdx.scenes.scene2d.ui.Label;
+import com.badlogic.gdx.scenes.scene2d.ui.Skin;
+import com.badlogic.gdx.scenes.scene2d.ui.Table;
+import com.badlogic.gdx.scenes.scene2d.ui.TextButton;
+import com.badlogic.gdx.utils.viewport.ScreenViewport;
+
+import inf112.bomberperson.game.BombermanGame;
+
+public class AbstractScreen {
+    
+}
