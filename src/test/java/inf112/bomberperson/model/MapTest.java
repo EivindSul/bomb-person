eeaae6b3372af92fd5159a3e64a067bc02e77ac0
kjell <xit014@uib.no>
2023-04-14 11:55:04 +0200

    some tests, but map is still empty

--- src/test/java/inf112/bomberperson/model/MapTest.java
+++ src/test/java/inf112/bomberperson/model/MapTest.java
@@ -5,6 +5,7 @@ import com.badlogic.gdx.graphics.OrthographicCamera;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
 import com.badlogic.gdx.maps.tiled.*;
 import com.badlogic.gdx.maps.tiled.renderers.OrthogonalTiledMapRenderer;
+import com.badlogic.gdx.scenes.scene2d.ui.Cell;
 import com.badlogic.gdx.utils.viewport.FitViewport;
 import com.badlogic.gdx.utils.viewport.Viewport;
 import org.junit.jupiter.api.*;
@@ -12,7 +13,7 @@ import org.junit.jupiter.api.*;
 import java.util.Random;
 
 import static org.junit.jupiter.api.Assertions.*;
-
+/*
 public class MapTest extends Map{
     private static final int TILE_SIZE = 32;
     static final int MAP_WIDTH = 27;
@@ -87,7 +88,7 @@ public class MapTest extends Map{
     }
 
     @Test
-    void notWrongCellType() {
+    void correctCellType() {
         Boolean test = true;
         for (int x = 0; x < Map.MAP_WIDTH; x++) {
             for (int y = 0; y < Map.MAP_HEIGHT; y++) {
@@ -170,3 +171,106 @@ public class MapTest extends Map{
         return layer.getCell(x, y);
     }
 }
+*/
+import org.junit.Before;
+import org.junit.Test;
+import org.mockito.Mock;
+import org.mockito.MockitoAnnotations;
+
+import static org.junit.Assert.assertEquals;
+import static org.mockito.Mockito.*;
+
+public class MapTest {
+
+    private Map map;
+
+    @Mock
+    private TiledMapTileLayer explodableWallLayer;
+    @Mock
+    private TiledMapTileLayer groundLayer;
+    @Mock
+    private TiledMapTileLayer wallLayer;
+    @Mock
+    private TiledMapTileSet tileSet;
+
+    @Before
+    public void setUp() {
+        MockitoAnnotations.initMocks(this);
+        map = spy(new Map());
+        map.explodableWallLayer = explodableWallLayer;
+    }
+
+    @Test
+    public void testTileType() {
+        Map.TileType tileType = Map.TileType.GRASS;
+        assertEquals("GRASS", tileType.name());
+    }
+
+    @Test
+    public void testGetWidth() {
+        float expected = 27f;
+        float actual = map.getWidth();
+        assertEquals(expected, actual, 0);
+    }
+
+    @Test
+    public void testGetHeight() {
+        float expected = 27f;
+        float actual = map.getHeight();
+        assertEquals(expected, actual, 0);
+    }
+
+    @Test
+    public void testGenerateMap() {
+        // Call the method to generate the map
+        map.generateMap();
+
+        // Check that the wall layer and explodable wall layer are not null
+        assertNotNull(map.wallLayer);
+        assertNotNull(map.explodableWallLayer);
+
+        // Check that the wall layer and explodable wall layer have the correct number of tiles
+        assertEquals(27, map.wallLayer.getWidth());
+        assertEquals(27, map.wallLayer.getHeight());
+        assertEquals(27, map.explodableWallLayer.getWidth());
+        assertEquals(27, map.explodableWallLayer.getHeight());
+
+        // Check that the ground layer has the correct number of tiles
+        assertEquals(27, map.groundLayer.getWidth());
+        assertEquals(27, map.groundLayer.getHeight());
+
+        // Check that each tile on the wall layer and explodable wall layer is either a wall or grass tile
+        for (int x = 0; x < map.wallLayer.getWidth(); x++) {
+            for (int y = 0; y < map.wallLayer.getHeight(); y++) {
+                TiledMapTileLayer.Cell cell = map.wallLayer.getCell(x, y);
+                if (cell != null) {
+                    TiledMapTile tile = cell.getTile();
+                    assertTrue(tile.getId() == Map.WALL_TILE_ID || tile.getId() == Map.GRASS_TILE_ID);
+                }
+
+                cell = map.explodableWallLayer.getCell(x, y);
+                if (cell != null) {
+                    TiledMapTile tile = cell.getTile();
+                    assertTrue(tile.getId() == Map.WALL_TILE_ID || tile.getId() == Map.GRASS_TILE_ID);
+                }
+            }
+        }
+
+        // Check that each tile on the ground layer is a grass tile
+        for (int x = 0; x < map.groundLayer.getWidth(); x++) {
+            for (int y = 0; y < map.groundLayer.getHeight(); y++) {
+                TiledMapTileLayer.Cell cell = map.groundLayer.getCell(x, y);
+                if (cell != null) {
+                    TiledMapTile tile = cell.getTile();
+                    assertEquals(Map.GRASS_TILE_ID, tile.getId());
+                }
+            }
+        }
+    }
+
+}
+
+
+
+
+
